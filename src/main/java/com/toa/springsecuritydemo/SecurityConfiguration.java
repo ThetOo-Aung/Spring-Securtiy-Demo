60b5ef733bbf2bd36ada59552aaed2de87895e6e
Thet Oo Aung <thetooaung07@gmail.com>
2021-01-02 23:11:11 +0630

    Allow user Role only

--- src/main/java/com/toa/springsecuritydemo/SecurityConfiguration.java
+++ src/main/java/com/toa/springsecuritydemo/SecurityConfiguration.java
@@ -2,26 +2,20 @@ package com.toa.springsecuritydemo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    //set your configuration on the auth object
+    // set your configuration on the auth object
 
-    auth.inMemoryAuthentication()
-        .withUser("thetooaung")
-        .password("thetooaung")
-        .roles("USER")
-        .and()
-        .withUser("toa2")
+    auth.inMemoryAuthentication().withUser("thetooaung").password("thetooaung").roles("USER").and().withUser("toa2")
         .password("toa2").roles("ADMIN");
   }
 
@@ -29,5 +23,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   public PasswordEncoder getPasswordEncoder() {
     return NoOpPasswordEncoder.getInstance();
   }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+  
+    http.authorizeRequests()
+    .antMatchers("/**").hasRole("USER").and().formLogin();
+  }
+
+  
     
 }
