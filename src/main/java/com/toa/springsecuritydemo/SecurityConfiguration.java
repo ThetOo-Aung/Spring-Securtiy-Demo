8fabb13f09ee21302f76356225b3264412c05e20
Thet Oo Aung <thetooaung07@gmail.com>
2021-01-07 00:36:20 +0630

    Add JWT

--- src/main/java/com/toa/springsecuritydemo/SecurityConfiguration.java
+++ src/main/java/com/toa/springsecuritydemo/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.toa.springsecuritydemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,9 +28,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   // authorization
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests().antMatchers("/admin").hasRole("ADMIN")
-        .antMatchers("/user").hasAnyRole("USER", "ADMIN").antMatchers("/")
-        .permitAll().and().formLogin();
+    http.csrf().disable().authorizeRequests().antMatchers("/authenticate")
+        .permitAll().anyRequest().authenticated();
+  }
+
+  @Override
+  @Bean
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
   }
 
   @Bean
